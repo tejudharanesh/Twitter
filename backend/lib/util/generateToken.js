5ad8823afcfc28bf8959e286c0c2c3553d6c5cc3
TEJU D <111564230+tejudharanesh@users.noreply.github.com>
2025-01-20 11:38:01 +0530

    Update generateToken.js

--- backend/lib/util/generateToken.js
+++ backend/lib/util/generateToken.js
@@ -6,8 +6,6 @@ export const generateTokenAndSetCookie = (userId, res) => {
   });
   res.cookie("token", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in milliseconds
-    httpOnly: true, //to prevent XSS attacks
-    sameSite: "strict", //CSRF protection
     secure: process.env.NODE_ENV === "production",
   });
 };

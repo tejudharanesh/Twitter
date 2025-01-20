b4ca56db63f3e3bbbc34ac015adde4057ea9187a
TEJU D <111564230+tejudharanesh@users.noreply.github.com>
2025-01-20 10:58:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true })); //to parse form data
 app.use(cookieParser()); //to parse cookies
 app.use(
   cors({
-    origin: "https://678ddc2d80a976374699f53b--legendary-churros-cab1af.netlify.app/", // Replace with your frontend's URL
+    origin: "https://678ddeddeec71241030e2990--celebrated-malabi-7de64c.netlify.app/", // Replace with your frontend's URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true, // If you need cookies or authentication headers
   })
